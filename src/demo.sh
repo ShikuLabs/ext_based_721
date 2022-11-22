425a5a1943f57fd25e18f9e7b582934c454cef0e
cymqqqq <648074155@qq.com>
2022-11-22 13:30:47 +0800

    update demo.sh

--- src/demo.sh
+++ src/demo.sh
@@ -2,6 +2,14 @@
 
 #$TOKEN2=$('dfx canister call ext_based_721_backend token_identifier 1')
 
+#dfx deploy --network ic --no-wallet ext_based_721_backend  --argument '(opt record{custodians=opt vec{principal "kkwoi-3jebw-6qx6z-yeah7-pgtlm-gbqdm-kkvyt-eqgbl-x3vpw-wfu2w-rqe"}; cap=opt principal "kkwoi-3jebw-6qx6z-yeah7-pgtlm-gbqdm-kkvyt-eqgbl-x3vpw-wfu2w-rqe"})'
+
+dfx canister call ext_based_721_backend batch_mint \
+"(record {to=variant {\"principal\"=principal \"gze77-i3egd-wbuoy-zn27p-wv5ze-casv2-w4miv-skbzu-eil5w-uacl7-xae\"};
+    metadata=opt vec {0};
+    class=\"H\";}, 
+      opt 3)"
+
 dfx canister  call ext_based_721_backend batch_transfer \
 "(record {to=variant {\"principal\"=principal \"ztbm7-6u6jt-hgprr-7qx56-p36ca-a2qvx-y5767-4dxuc-xllss-ebtpt-sae\"};
     token= \"nelid-eakor-uwiaa-aaaaa-aaaaa-eaqca-aaaaa-q\";
@@ -11,3 +19,4 @@ dfx canister  call ext_based_721_backend batch_transfer \
      subaccount=opt vec {0};
       amount=1;},
       opt 3)"
+
